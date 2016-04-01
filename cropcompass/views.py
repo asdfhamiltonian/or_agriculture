1b2dcae393b02f4b84fd1b7e75ebbd78318f20ee
Danielpearl <danielaaronpearl@gmail.com>
2016-03-31 18:24:42 -0700

    django cropcompass app created

---
+++ cropcompass/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+from django.http import HttpResponse, HttpResponseRedirect
+from django.contrib.auth import authenticate, login, logout
+
+def home_page(request):
+    """
+    Displays home page
+    """
+    pass
